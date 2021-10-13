79970d2a8b23c731487ee2b942ef378fb78e68eb
Lucas Pontes <lucasdev@pop-os.localdomain>
2021-10-13 16:24:58 -0300

    Mexendo com variáveis.

--- Program.cs
+++ Program.cs
@@ -9,6 +9,24 @@ namespace primeiro_projeto_dotnet
             var teste = new Teste();
             Console.WriteLine("Hello World!");
             teste.primeiroTeste();
+
+            int idade;
+            idade = 32;
+            Console.WriteLine(idade);
+
+            idade = 10;
+            Console.WriteLine(idade);
+
+            idade = idade + 5;
+            Console.WriteLine("Sua idade é: " + idade);
+
+            idade = 10 + 5 * 2;
+            Console.WriteLine("Sua idade é " + idade + " ?");
+
+            idade =  (10 + 5) * 2;
+            Console.WriteLine(idade);
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair!!");
             Console.ReadLine();
         }
     }
